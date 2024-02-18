2da40192ec19c718a15db731faa08ba09e4d98fa
calebryan.corrigibility@gmail.com <calebryan.corrigibility@gmail.com>
2024-02-17 18:27:38 -0600

    Add project files.

---
+++ FirstBloonsMod.cs
@@ -0,0 +1,32 @@
+﻿using MelonLoader;
+using BTD_Mod_Helper;
+using FirstBloonsMod;
+using Il2CppAssets.Scripts.Simulation.Towers;
+using Il2CppAssets.Scripts.Simulation.Objects;
+using Il2CppAssets.Scripts.Models;
+
+[assembly: MelonInfo(typeof(FirstBloonsMod.FirstBloonsMod), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace FirstBloonsMod;
+
+public class FirstBloonsMod : BloonsTD6Mod
+{
+    public override void OnApplicationStart()
+    {
+        ModHelper.Msg<FirstBloonsMod>("FirstBloonsMod loaded!");
+    }
+    public override void OnTowerCreated(Tower tower, Entity target, Model modelToUse)
+    {
+        if (tower.towerModel.name.Contains("EngineerMonkey"))
+        {
+            ModHelper.Msg<FirstBloonsMod>("EngineerMonkey created!");
+        }
+        else if (tower.towerModel.name.Contains("TechBot"))
+        {
+            ModHelper.Msg<FirstBloonsMod>("TechBot created!");
+        }
+        base.OnTowerCreated(tower, target, modelToUse);
+    }
+
+}
---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+﻿namespace FirstBloonsMod;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "40.0";
+    public const string Version = "1.0.0";
+    public const string Name = "FirstBloonsMod";
+
+    public const string Description = "An empty mod";
+
+    public const string RepoOwner = "calebRyanHenry"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "FirstBloonsMod"; // TODO add your repo name here, also in the download url in README.md
+}
